edc8eb8b4c343c52a3558523f0211b77254b4d3d
Mahesh <maheshchoudhary028@gmail.com>
2018-07-07 22:52:36 +0530

    Base App2

--- app/src/main/java/com/viinman/product/vachunt/view/profile/ProfileActivity.java
+++ app/src/main/java/com/viinman/product/vachunt/view/profile/ProfileActivity.java
@@ -6,16 +6,29 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.viinman.product.vachunt.R;
 
 public class ProfileActivity extends Fragment {
+
+    Button leaderBtn, contactBtn, eventBtn, bgBtn, locBtn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.profile_tab, container, false);
 
+        leaderBtn =rootView.findViewById(R.id.profile_leadershipBtn);
+        bgBtn =rootView.findViewById(R.id.profile_backgroundBtn);
+        eventBtn =rootView.findViewById(R.id.profile_eventsBtn);
+        contactBtn =rootView.findViewById(R.id.profile_contactsBtn);
+        locBtn =rootView.findViewById(R.id.profile_locationBtn);
+
+
         return  rootView;
+    }
+    public void onLeadershipClick(View view){
+
     }
 }
