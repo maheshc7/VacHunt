8b1aebfcac3d6da4fa4d6c8afb5757e0c76b39ef
Vijaay <vijaaykumaaresan@gmail.com>
2018-07-07 19:31:58 +0530

    Process Design 1

--- app/src/main/java/com/viinman/product/vachunt/view/hunt/HuntActivity.java
+++ app/src/main/java/com/viinman/product/vachunt/view/hunt/HuntActivity.java
@@ -1,12 +1,15 @@
 package com.viinman.product.vachunt.view.hunt;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.viinman.product.vachunt.R;
+import com.viinman.product.vachunt.view.hunt.process.ProcessShortlist1;
 
 public class HuntActivity extends Fragment {
 
@@ -14,6 +17,14 @@ public class HuntActivity extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.hunt_tab, container, false);
+        Button process = rootView.findViewById(R.id.hunt_processBtn);
+        process.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toProcess = new Intent(getContext(), ProcessShortlist1.class);
+                startActivity(toProcess);
+            }
+        });
 
         return  rootView;
     }
---
+++ app/src/main/java/com/viinman/product/vachunt/view/hunt/process/ProcessShortlist2.java
@@ -0,0 +1,15 @@
+package com.viinman.product.vachunt.view.hunt.process;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.viinman.product.vachunt.R;
+
+public class ProcessShortlist2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_process_shortlist_2);
+    }
+}
