459a27883fb08516e16213e9786f9f21a8948937
Vijaay <vijaaykumaaresan@gmail.com>
2018-07-08 13:17:21 +0530

    Process Design 2

---
+++ app/src/main/java/com/viinman/product/vachunt/ProcessDeployment1.java
@@ -0,0 +1,13 @@
+package com.viinman.product.vachunt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProcessDeployment1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_process_deployment1);
+    }
+}
